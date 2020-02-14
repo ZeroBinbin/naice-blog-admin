928a7cb5b72199feb1d3f57d8b13f5d3ff0fbdb1
xuexiaofeng <xuexiaofeng@chinasofti.com>
2020-02-14 10:05:50 +0800

    change api url

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,3 +1,3 @@
 export const IS_DEV:boolean = process.env.NODE_ENV !== 'production'
 
-export const API_ROOT:string = IS_DEV ? 'http://localhost:3009/api/' : 'https://blogapi.naice.me/api/'
+export const API_ROOT:string = IS_DEV ? 'http://localhost:3009/api/' : 'https://106.13.150.151/api/'
